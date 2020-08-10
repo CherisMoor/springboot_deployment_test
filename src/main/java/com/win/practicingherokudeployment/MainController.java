8d0e8f18a6bfaec6b1e8821523ac1f6e23cc6078
Cherish <62115637+CherisMoor@users.noreply.github.com>
2020-08-10 08:36:59 -0400

    first commit

---
+++ src/main/java/com/win/practicingherokudeployment/MainController.java
@@ -0,0 +1,12 @@
+package com.win.practicingherokudeployment;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    
+    @GetMapping("/")
+    public String sayHello(){
+        return "hello world";
+    }
+}
