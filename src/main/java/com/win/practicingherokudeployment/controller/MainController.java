d7266be4a685af38ca0af9cf7ed59f70f0e0fba2
Cherish <62115637+CherisMoor@users.noreply.github.com>
2020-08-10 08:40:39 -0400

    first commit

--- src/main/java/com/win/practicingherokudeployment/controller/MainController.java
+++ src/main/java/com/win/practicingherokudeployment/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.win.practicingherokudeployment;
+package com.win.practicingherokudeployment.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
